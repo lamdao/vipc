7f90cc52b777c1f3ac494b66588b42edecd19dfe
Lam H. Dao <daohailam@yahoo.com>
2015-10-13 12:27:12 -0400

    Added back thread affinity and permission

--- Core/dthread.h
+++ Core/dthread.h
@@ -1,6 +1,11 @@
 #ifndef __DTHREAD_H
 #define __DTHREAD_H
 //--------------------------------------------------------------------------
+#include <mutex>
+#include <condition_variable>
+//--------------------------------------------------------------------------
+using namespace std;
+//--------------------------------------------------------------------------
 static size_t load;		// workload
 //--------------------------------------------------------------------------
 namespace DThread {
@@ -10,10 +15,45 @@ static int NUM_THREADS = omp_get_max_threads();
 #else
 static int NUM_THREADS = thread::hardware_concurrency();
 #endif
+static const int MAX_NUM_THREADS = thread::hardware_concurrency();
 //--------------------------------------------------------------------------
 static inline void CalcWorkload(size_t total)
 {
 	load = (total / NUM_THREADS) + ((total % NUM_THREADS) > 0);
+}
+//--------------------------------------------------------------------------
+class Permission
+{
+private:
+	bool ok = false;
+	condition_variable cv;
+	mutex mtx;
+public:
+	void wait() {
+		unique_lock<mutex> lock(mtx);
+		cv.wait(lock, [this] { return ok; });
+	}
+	void allow() {
+		ok = true;
+		cv.notify_all();
+	}
+	void clear() {
+		ok = false;
+	}
+};
+//--------------------------------------------------------------------------
+inline void SetAffinity(thread &thr, int n)
+{
+#ifdef USE_AFFINITY
+#ifdef _WIN32
+	SetThreadAffinityMask((HANDLE)thr.native_handle(), 1<<(n%MAX_NUM_THREADS));
+#else
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(n % MAX_NUM_THREADS, &cpuset);
+	pthread_setaffinity_np(thr.native_handle(), sizeof(cpu_set_t), &cpuset);
+#endif
+#endif
 }
 //--------------------------------------------------------------------------
 template<class Functor>
@@ -26,11 +66,20 @@ static inline void Start(Functor action)
 	}
 #else
 	static vector<thread> workers(NUM_THREADS);
+	static Permission permission;
 	int __thread_id = 0;
+
+	permission.clear();
 	for (auto &w : workers) {
-		w = thread(action, __thread_id);
+		w = thread([&](int __thread_id) {
+				permission.wait();
+				action(__thread_id);
+			}, __thread_id);
+		SetAffinity(w, __thread_id);
 		__thread_id++;
 	}
+
+	permission.allow();
 	for (auto &w : workers) {
 		w.join();
 	}
